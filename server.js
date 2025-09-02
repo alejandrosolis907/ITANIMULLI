51537458098aa1da7eb3fbde73c35b9ddc61ce8b
alejandrosolis907 <alexsoli3579@gmail.com>
2025-09-01 21:06:47 -0600

    Refactor server listen block

--- server.js
+++ server.js
@@ -10,4 +10,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
