661eece6f976581646d6649147938646b8d40166
alejandrosolis907 <alexsoli3579@gmail.com>
2025-09-01 20:37:44 -0600

    Use dynamic port variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000; // use Railway PORT or fallback to 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
@@ -10,6 +10,4 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
